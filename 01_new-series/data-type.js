3cb2de9438a339db99e844b07977ad49aaf1e983
KirtiThakur123 <51044378+KirtiThakur123@users.noreply.github.com>
2026-07-02 07:28:01 +0000

    changes-push

---
+++ 01_new-series/data-type.js
@@ -0,0 +1,15 @@
+// Intiger
+// string 
+// Float
+// null : object (standalone value) is basically ek object hai
+// undefined : data type (empty value) is basically ek data type hai.
+
+// typeof function btata hgai type of the data and we can also change the type   according to our need using
+
+
+let score = 123
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = String(score)
+console.log(typeof valueInNumber );
---
+++ 01_new-series/variable.js
@@ -0,0 +1,17 @@
+const accountId = 12
+let accountEmail = "kirti234@gmail.com"
+let accountState;
+var accountPassword = "1234"
+accountCity = "jaipur"
+
+
+
+accountEmail = "muskansharmaup@gmail.com"
+accountPassword = "12345"
+accountCity = "mandi" 
+
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+// Java script me hmesha const se constant declare hote hai and variable ko hmesha Let se declare krna hai.
